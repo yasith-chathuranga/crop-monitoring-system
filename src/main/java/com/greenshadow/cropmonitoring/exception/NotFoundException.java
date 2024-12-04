adb2b8bc5fb0990dc6fb4e3e45a46d034c594ef2
yasith-chathuranga <yasithchathuranga999@gmail.com>
2024-12-04 20:52:23 +0530

    not found exception added

---
+++ src/main/java/com/greenshadow/cropmonitoring/exception/NotFoundException.java
@@ -0,0 +1,12 @@
+package com.greenshadow.cropmonitoring.exception;
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException() {
+    }
+
+    public NotFoundException(String message) {
+    }
+
+    public NotFoundException(String message, Throwable cause) {
+    }
+}
